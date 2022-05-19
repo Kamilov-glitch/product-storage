d3b9c165789612747f0f2e38524281c475db65fd
Fuad <kamilovfuad@gmail.com>
2022-05-19 10:07:15 +0400

    Add buttons to the index page

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once "partials/header.php";
+
+?>
+
+<div class="container">
+    <div class="d-flex flex-row justify-content-end pt-5">
+            <a href="" class="btn btn-success pe-4 me-2" role="button">ADD</a>
+            <a href="" class="btn btn-primary ms-2" role="button">MASS DELETE</a>
+    </div>
+</div><br/>
+
+<div>
+    <hr/>
+</div>
+
+
+
+<?php 
+
+include_once "partials/footer.php"
+
+?>
