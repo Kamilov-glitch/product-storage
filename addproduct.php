7b2d8f3d6c14401fe0a5b0a4cf22c92339a004ef
Fuad <kamilovfuad@gmail.com>
2022-05-20 10:31:18 +0400

    Add button to addproduct page

--- addproduct.php
+++ addproduct.php
@@ -6,6 +6,13 @@ include_once "partials/header.php";
 
 <div class="container pt-5">
     <form action="" id="product_form">
+        <div class="container">
+            <h2>Product Add</h2>
+            <div class="d-flex flex-row justify-content-end pt-2">
+                <button class="btn btn-success pe-4 me-2" type="submit">Save</button>
+                <a href="index.php" class="btn btn-primary ms-2" role="button">Cancel</a>
+            </div>
+        </div><br/>
         <div class="form-group">
             <label for="sku" class="pe-4">SKU</label>
             <input type="text" name="sku" id="sku" class="form-control">
