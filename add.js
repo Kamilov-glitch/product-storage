37164b2a50b3d24682178090876e38f3fb8dcdc4
Fuad <kamilovfuad@gmail.com>
2022-05-21 21:08:50 +0400

    Add new fields according to the chosen type of product

--- add.js
+++ add.js
@@ -1,7 +1,28 @@
 $(document).ready(() => {
     $('#productType').change(function()
 {
-    $('#dynamic').html('<input type="text" step="0.01" class="form-control" name="height"></input>');
+    switch(this.value) {
+        case 'dvd':
+            $('#dynamic').html("<label for='size'>Size</label>" +
+                "<input type='text' class='form-control' name='size'>"
+            );
+            break;
+        case 'book':
+            $('#dynamic').html("<label for='weight'>Weight</label>" +
+                "<input type='text' class='form-control' name='weight'>"
+            );
+            break;
+        case 'furniture':
+            $('#dynamic').html("<label for='height'>Height</label>" +
+                "<input type='text' class='form-control' name='height'>" +
+                "<label for='width'>Width</label>" +
+                "<input type='text' class='form-control' name='width'>" +
+                "<label for='length'>Length</label>" +
+                "<input type='text' step='0.01' class='form-control' name='length'>"
+            );
+            break;
+    }
+    
 });
 })
 
